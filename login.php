a319ae38b9405618724a61e005445d5e4723869b
AbedlHakimR <165525046+AbedlHakimR@users.noreply.github.com>
2024-03-31 00:23:13 +0000

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 	$email = $_POST['email'];
     $password = $_POST['password'];
 
-    $con = new mysqli('localhost','root','','test');
+    $con = new mysqli('185.199.110.153','root','','test');
     if($con->connect_error){
         die('Connection Failed : '.$con->connect_error);
     }else{
